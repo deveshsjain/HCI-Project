37b3104ead294cb68ad7bd3082d6e1e809068e3e
deveshsjain <42722660+deveshsjain@users.noreply.github.com>
2019-04-16 13:55:47 -0400

    Updates

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ const homeRoutes = require("./home");
 const deposit = require("./deposit");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
-const expenses = require("./expenses");
-const dashboard = require("./dashboard");
+const expenses = require("./expenses")
+const dashboard = require("./dashboard")
 const constructorMethod = app => {
 
   app.use(cookieParser());
@@ -11,10 +11,9 @@ const constructorMethod = app => {
   app.use(bodyParser.json());
   try {
     app.use("/", homeRoutes);
-    app.use("/dashboard", dashboard);
     app.use("/deposit", deposit);
     app.use("/expenses", expenses);
-
+    app.use("/dashboard", dashboard);
   }
   catch (error) {
     console.log(error);
